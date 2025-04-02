27109bfd00a5e3d238bbe5432ee62e046453be63
hugocis <9101917@alumnos.ufv.es>
2025-04-02 20:30:37 +0200

    Update Hardhat configuration: set Solidity version to 0.8.19 and define network paths

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -2,7 +2,19 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
 
 export default config;
