30fda0aedf04d49723e367ecffe5cc74c781e11c
hugocis <9101917@alumnos.ufv.es>
2025-04-02 21:52:04 +0200

    refactor: update SimpleStorage contract and interface to support value accumulation and detailed event logging

--- blockchain/contracts/SimpleStorage.sol
+++ blockchain/contracts/SimpleStorage.sol
@@ -3,27 +3,28 @@ pragma solidity ^0.8.0;
 
 /**
  * @title SimpleStorage
- * @dev A simple contract to store and retrieve a value
+ * @dev A simple contract to store and accumulate values
  */
 contract SimpleStorage {
-    // Variable to store the value
+    // Variable to store the accumulated value
     uint256 private storedValue;
 
-    // Event emitted when the value is changed
-    event ValueChanged(uint256 newValue);
+    // Event emitted when the value is changed, showing old and new values
+    event ValueChanged(uint256 oldValue, uint256 newValue, uint256 addedValue);
 
     /**
-     * @dev Store a new value
-     * @param value The value to store
+     * @dev Add a new value to the existing stored value
+     * @param value The value to add to the existing stored value
      */
     function store(uint256 value) public {
-        storedValue = value;
-        emit ValueChanged(value);
+        uint256 oldValue = storedValue;
+        storedValue += value;
+        emit ValueChanged(oldValue, storedValue, value);
     }
 
     /**
      * @dev Retrieve the stored value
-     * @return The current stored value
+     * @return The current accumulated value
      */
     function retrieve() public view returns (uint256) {
         return storedValue;
