38564ea3fd87bcc362f72ae3044af2eb3d5dc82f
hugocis <9101917@alumnos.ufv.es>
2025-04-06 22:51:15 +0200

    feat: Refactor Hardhat configuration to include accounts settings for localhost and hardhat networks

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -15,14 +15,25 @@ const config: HardhatUserConfig = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        accountsBalance: "10000000000000000000000" // 10000 ETH por cuenta
+      }
     },
     hardhat: {
       mining: {
         auto: true,
         interval: 1000
       },
-      chainId: 31337
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        accountsBalance: "10000000000000000000000" // 10000 ETH por cuenta
+      }
     }
   },
   paths: {
